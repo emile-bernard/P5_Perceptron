3aebb7c3b400bdd1a4f0da655a64bead4224441c
Emile555 <emile.bernard.2@ulaval.ca>
2018-12-24 00:17:24 -0500

    Cleaned up code

--- sketch.js
+++ sketch.js
@@ -13,12 +13,6 @@ let ymin = -1;
 let xmax = 1;
 let ymax = 1;
 
-// The function to describe a line
-function lineDescription(x) {
-    let y = 0.3 * x + 0.4;
-    return y;
-}
-
 function setup() {
     createCanvas(600, 600);
 
@@ -31,19 +25,6 @@ function setup() {
     createRandomPointSet();
 }
 
-function createRandomPointSet() {
-    // Create a random set of training points and calculate the "known" answer
-    for (let i = 0; i < trainingPoints.length; i++) {
-        let x = random(xmin, xmax);
-        let y = random(ymin, ymax);
-        let answer = 1;
-
-        if (y < lineDescription(x)) answer = -1;
-        trainingPoints[i] = new Point(x, y, 1, answer);
-    }
-}
-
-
 function draw() {
     background(0);
 
@@ -53,7 +34,6 @@ function draw() {
     trainPerceptron();
 
     drawPoints();
-
     drawStatusText();
 }
 
@@ -62,20 +42,35 @@ function drawStatusText() {
     document.getElementById('count-status-text').innerText = 'Count: ' + count.toString() + '/2000';
 }
 
-function trainPerceptron() {
-    // Train the Perceptron with one "training" point at a time
-    perceptron.train(trainingPoints[count].getInputs(), trainingPoints[count].getOutput());
-    count = (count + 1) % trainingPoints.length;
+function createRandomPointSet() {
+    // Create a random set of training points and calculate the "known" answer
+    for (let i = 0; i < trainingPoints.length; i++) {
+        let x = random(xmin, xmax);
+        let y = random(ymin, ymax);
+        let label = 1;
+        let answer = 1;
+
+        if (y < lineDescription(x)) answer = -1;
+        trainingPoints[i] = new Point(x, y, label, answer);
+    }
+}
+
+// The function to describe a line
+function lineDescription(x) {
+    let y = 0.3 * x + 0.4;
+    return y;
 }
 
 function drawLine() {
     // Draw the line
-    strokeWeight(1);
     stroke(255);
+    strokeWeight(1);
+
     let x1 = map(xmin, xmin, xmax, 0, width);
     let y1 = map(lineDescription(xmin), ymin, ymax, height, 0);
     let x2 = map(xmax, xmin, xmax, 0, width);
     let y2 = map(lineDescription(xmax), ymin, ymax, height, 0);
+
     line(x1, y1, x2, y2);
 }
 
@@ -95,6 +90,7 @@ function drawCurrentWeightsLine() {
     y1 = map(y1, ymin, ymax, height, 0);
     x2 = map(x2, xmin, xmax, 0, width);
     y2 = map(y2, ymin, ymax, height, 0);
+
     line(x1, y1, x2, y2);
 }
 
@@ -104,4 +100,10 @@ function drawPoints() {
     for (let i = 0; i < count; i++) {
         trainingPoints[i].drawPoint();
     }
+}
+
+function trainPerceptron() {
+    // Train the Perceptron with one "training" point at a time
+    perceptron.train(trainingPoints[count].getInputs(), trainingPoints[count].getOutput());
+    count = (count + 1) % trainingPoints.length;
 }
